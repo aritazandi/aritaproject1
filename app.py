49b9d73cc0b092d18a8bdc56bdede1fc410cee3a
arita <124104899+aritazandi@users.noreply.github.com>
2025-11-02 20:56:37 +0330

    initial commit with CI/CD pipeline

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return " welcome to my first website(arita zandi)"
+if __name__ == '__main__':
+    app.run(debug=True)
+print("hi")
